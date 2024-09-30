bc0a939f8d9a3020e54d49539f9274143ac07569
borystarabarov <borys.tarabarov@gmail.com>
2024-09-30 20:01:03 +0300

    updates

---
+++ my_scripts/Classes/Intro.py
@@ -0,0 +1,19 @@
+class cuboid:
+    def __init__(self, l,b,h):
+        self.length = l
+        self.breadth = b
+        self.hight = h
+        self.volume_attr = self.length*self.breadth*self.hight
+    
+    def lidarea(self):
+        return self.length*self.breadth
+    
+    def volume(self):
+        return (self.length*self.breadth*self.hight)
+    
+
+c1 = cuboid(1,2,3)
+
+print(c1.hight)
+print(c1.volume())
+print(c1.volume_attr)
